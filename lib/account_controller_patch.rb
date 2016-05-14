303dc33b057a3497b3e0a0d8333b8a486d5cbdf0
bilel-kedidi <bilel.kedidi@gmail.com>
2016-05-14 16:52:52 +0100

    add recaptcha

--- lib/account_controller_patch.rb
+++ lib/account_controller_patch.rb
@@ -8,7 +8,7 @@ module AccountControllerRecaptchaPatch
             redirect_back_or_default home_url, :referer => true
           end
         else
-          if Setting.plugin_recaptcha['recaptcha_private_key'].nil? or verify_recaptcha(:model => @user, :private_key => Setting.plugin_recaptcha['recaptcha_private_key'])
+          if Setting.plugin_recaptcha['recaptcha_private_key'].presence or verify_recaptcha(:model => @user, :private_key => Setting.plugin_recaptcha['recaptcha_private_key'])
             authenticate_user
           end
         end
